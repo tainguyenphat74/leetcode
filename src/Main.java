8f8881e195852f6e46bbef757ea58645c0c22ff0
tainguyenphat74 <nguyentai2760@gmail.com>
2023-02-11 20:52:30 +0700

    [Easy] No9

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import leetcode.solution.easy.no9.No9;
+
+public class Main {
+    public static void main(String[] args) {
+        No9 solution = new No9();
+        System.out.println(String.format("%s", solution.isPalindrome(1234567899)));
+    }
+}
+
---
+++ src/leetcode/solution/easy/no9/No9.java
@@ -0,0 +1,30 @@
+package leetcode.solution.easy.no9;
+
+public class No9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x < 10) return true;
+
+        String c = "";
+        int o = x;
+
+        while (x != 0) {
+            int t = x % 10;
+            x = x / 10;
+            c = c.concat(String.valueOf(t));
+        }
+
+        int s_to_i;
+
+        try {
+            // -2^31 <= c <= 2^31 - 1
+            s_to_i = Integer.parseInt(c);
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (s_to_i == o) return true;
+
+        return false;
+    }
+}
